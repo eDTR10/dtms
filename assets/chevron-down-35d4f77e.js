f0a7aede40f9510f4b263f15cb52acae3513cb8c
iannico322 <iannicocaulin@gmail.com>
2026-08-26 16:15:34 +0800

    Updates

---
+++ assets/chevron-down-35d4f77e.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-7466e485.js";const n=o("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);export{n as C};
---
+++ assets/user-52cbda8e.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-7466e485.js";const r=e("Briefcase",[["rect",{width:"20",height:"14",x:"2",y:"7",rx:"2",ry:"2",key:"eto64e"}],["path",{d:"M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16",key:"zwj3tp"}]]),a=e("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]);export{r as B,a as U};
